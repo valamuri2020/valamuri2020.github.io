ba18c30736b6c6da4059863f5ca1461da2cf5416
Vivek Alamuri <vivek.alamuri@gmail.com>
2020-09-17 11:28:23 -0400

    refactored main.js

--- main.js
+++ main.js
@@ -1,30 +1,4 @@
- function trees (){  // Display when button is clicked//
-    document.getElementById("hiddenmessage").style.display = 'block';
-} 
-
-//Update
-var t = 0;
-var constant = 475.646879756;
-
-function addtrees(){
-    trees();
-    t =  Math.ceil(t + constant/4);
-    document.getElementById("label").innerHTML = t ;
-}
-
-//jQuery used to hide initial stuff onclick
-$(document).ready(function(){
-    $("button").click(function(){
-        $(".begone").fadeOut(500)
-        
-     });
-
- }) ;
-
-
-/* Refactored Version:
-
- const trees =  ()=> document.getElementById("hiddenmessage").style.display = 'block'; 
+const trees =  ()=> document.getElementById("hiddenmessage").style.display = 'block'; 
 
 //Update count
 let t = 0;
@@ -33,13 +7,13 @@ let constant = 475.646879756;
 const addtrees = ()=> {
     trees();
     t = Math.ceil(t + constant/4);
-    $('#label').text(t)
+    $('#label').text(t);
 }
 
 //jQuery used to hide initial stuff onclick
 $(document).ready(()=>{
     $("button").click(()=>{
-        $(".begone").fadeOut(500)
+        $(".begone").fadeOut(500);
      });
  });
-*/
+
